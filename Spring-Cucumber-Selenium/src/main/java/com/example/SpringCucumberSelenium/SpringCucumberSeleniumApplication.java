ea9b70b7e3182ab4706a5d7b248e33a9e11950bf
meher28 <mehermeka@gmail.com>
2021-05-16 11:34:59 +0100

    initial checkin

---
+++ Spring-Cucumber-Selenium/src/main/java/com/example/SpringCucumberSelenium/SpringCucumberSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringCucumberSelenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCucumberSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCucumberSeleniumApplication.class, args);
+	}
+
+}
---
+++ Spring-Cucumber-Selenium/src/test/java/com/example/SpringCucumberSelenium/SpringCucumberSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCucumberSelenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCucumberSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
